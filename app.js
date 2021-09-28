4ad28d9cd0f941b756617ba95dabd605bbfa8c66
phobik <phobik2000@gmail.com>
2021-09-28 20:10:01 +0100

    fix

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 var express = require("express");
 var path = require("path");
-//var cookieParser = require("cookie-parser");
-//var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
-//app.use(logger("dev"));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function ignoreFavicon(req, res, next) {
-  if (req.originalUrl.includes('favicon.ico')) {
-    res.status(204).end()
+  if (req.originalUrl.includes("favicon.ico")) {
+    res.status(204).end();
   }
   next();
 }
 app.use(ignoreFavicon);
 
-
-
-var listener = app.listen(8080, function() {
+var listener = app.listen(8080, function () {
   console.log("Listening on port " + listener.address().port);
 });
